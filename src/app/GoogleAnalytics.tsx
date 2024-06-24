89c6a44743b6e1f31975ee2e0912d6e32f296c85
恒成立 <wo51study@163.com>
2024-06-25 00:28:14 +0800

    [需求] 1.Google分析

---
+++ src/app/GoogleAnalytics.tsx
@@ -0,0 +1,24 @@
+import Script from "next/script";
+import * as gtag from "@/constants/gtag.ts";
+
+const GoogleAnalytics = () => {
+    return (
+        <div>
+            <Script strategy={"afterInteractive"} src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}/>
+            <Script
+                id="gtag-init"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${gtag.GA_TRACKING_ID}', {
+                            page_path: window.location.pathname,
+                            });`,
+                }}
+            />
+        </div>
+    )
+}
+
+export default GoogleAnalytics;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import {Inter} from "next/font/google";
 import "./globals.css";
 import AppHeader from "@/components/AppHeader";
 import AppFooter from "@/components/AppFooter";
+import GoogleAnalytics from "@/app/GoogleAnalytics";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -23,6 +24,7 @@ export default function RootLayout(
         <AppHeader/>
         {children}
         <AppFooter/>
+        <GoogleAnalytics/>
         </body>
         </html>
     );
